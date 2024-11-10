455b9e7a73b847e85737c9c14d129cd8947be65d
Anandbk03 <122600480+Anandbk03@users.noreply.github.com>
2024-11-10 20:09:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const chatbotToggler = document.querySelector(".chatbot-toggler");
+econst chatbotToggler = document.querySelector(".chatbot-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = // Paste your API key here
+const API_KEY = // Enter your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
